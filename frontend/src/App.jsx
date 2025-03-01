48ae70900977ef945bbfaecd4fddaf97674d2776
Kush <jangidkush1234@gmail.com>
2025-03-02 00:03:47 +0530

    compnent sturctured

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import Quiz from './components/Quiz'
+
+function App() {
+  return <div className='box-border bg-[#001e4d] h-screen'>
+  <Quiz/>
+  </div>
+
+}
+
+export default App
---
+++ frontend/src/components/Quiz.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+import React from 'react'
+
+function Quiz() {
+  return (
+    <div className='bg-black w-[90%] max-w-[600px] mt-[100px] ml-auto mr-auto rounded-[10px] p-[30px]'>
+      
+    </div>
+  )
+}
+
+export default Quiz
